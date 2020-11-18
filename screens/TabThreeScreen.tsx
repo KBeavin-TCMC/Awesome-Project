9af7a0c7f87870aa93352ea4e4f3e951e3559832
Kyle Beavin <kyle.beavin@tcmcllc.com>
2020-11-18 13:34:09 -0500

    init commit

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -4,13 +4,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function TabOneScreen() {
+export default function TabThreeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <Text style={styles.title2}>Awww Yeah!</Text>
+      <Text style={styles.title}>Tab Three</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.js" />
+      <EditScreenInfo path="/screens/TabThreeScreen.js" />
     </View>
   );
 }
@@ -23,10 +22,6 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  title2: {
-    fontSize: 60,
     fontWeight: 'bold',
   },
   separator: {
